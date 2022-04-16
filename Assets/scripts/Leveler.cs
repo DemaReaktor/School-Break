ddf9757e9433751f1760cee44f6820bbea8e0e65
Dema <67798322+DemaReaktor@users.noreply.github.com>
2022-04-16 21:41:55 +0300

    Pupil
    Created prefab Pupil. Also in Leveler created spawn code for pupils.

--- Assets/scripts/Leveler.cs
+++ Assets/scripts/Leveler.cs
@@ -6,13 +6,16 @@ public class Leveler
     private static int level;
     private static int lenth;
     private static int width;
+    private static int pupilCount;
 
     public static int Lenth { get { return lenth; } }
     public static int Width { get { return width; } }
+    public static int PupilCount { get { return pupilCount; } }
     public static void Start()
     {
         level = 1;
         lenth = level*3 + 20;
         width = level / 10 + 3;
+        pupilCount =  (level-1)*3 + 1;
     }
 }
--- Assets/scripts/SceneLoader.cs
+++ Assets/scripts/SceneLoader.cs
@@ -2,8 +2,27 @@
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private GameObject pupil;
+    [SerializeField] private GameObject pupilFolder;
     private void Start()
     {
         Leveler.Start();
+        for (int index = 0; index < Leveler.PupilCount; index++)
+        {
+            Vector3 position = new Vector3(Random.Range(0, Leveler.Lenth),0,
+                    Random.Range(0, Leveler.Width));
+
+            for (int pupilIndex = 0; pupilIndex < index; pupilIndex++)
+            {
+                if (position.Equals(pupilFolder.transform.
+                    GetChild(pupilIndex).transform.position))
+                {
+                    position = new Vector3(Random.Range(0, Leveler.Lenth),0,
+                        Random.Range(0, Leveler.Width));
+                    pupilIndex = 0;
+                }
+            }
+            Instantiate(pupil, position, new Quaternion(), pupilFolder.transform);
+        }
     }
 }
