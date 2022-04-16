b261ba76b50fdf20f34abeb25267725e04adb279
Dema <67798322+DemaReaktor@users.noreply.github.com>
2022-04-16 20:52:36 +0300

    Move
    Created script PlayerController. Player can move forward and back.

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+   [SerializeField] [Range(0.05f,10)]
+    private float speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            this.transform.position+=new Vector3(-speed,0,0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            this.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+    }
+}
