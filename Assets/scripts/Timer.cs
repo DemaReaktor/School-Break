ec1d842b1326a1012eb0c447cf00611294c1dcd0
Dema <67798322+DemaReaktor@users.noreply.github.com>
2022-04-23 01:45:46 +0300

    EventHanlers for Timer
    Added EventHanlers( start, tick, end, finish).

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
-//using System.Threading;
 using System.Threading.Tasks;
-
+using System;
 public static class Timer
 {
     public static float TimeLeft { get { return time; } }
+    public static bool IsRuned{ get { return isTicked; } }
 
-    private static float time=0;
-    private static bool isTicked=false;
+    private static float time;
+    private static bool isTicked;
+    private static EventHandler startHandler;
+    private static EventHandler tickHandler;
+    private static EventHandler endHandler;
+    private static EventHandler finishHandler;
 
     public static void Start(float time) {
         isTicked = true;
         Timer.time = time;
         Tick();
+        startHandler?.Invoke(null,new GeneralEventArgs<float>(time)) ;
     }
     private static async void Tick() {
         while (time > 0f)
@@ -21,14 +26,30 @@ public static class Timer
                 return;
             await Task.Delay(10);
             time -= 0.01f;
+            tickHandler?.Invoke(null, new GeneralEventArgs<float>(time));
         }
 
         //end
         time = 0f;
+        endHandler?.Invoke(null,null);
     }
-    public static void Finish()
+    public static void Finish(object finisher)
     {
-        time = 0f;
         isTicked = false;
+        finishHandler?.Invoke(finisher, new GeneralEventArgs<float>(time));
+        time = 0f;
     }
+    public static void AddActionToStart(EventHandler action)
+        => startHandler += action;
+    public static void AddActionToTick(EventHandler action) 
+        => tickHandler += action;
+    public static void AddActionToEnd(EventHandler action)
+        => endHandler += action;
+    public static void AddActionToFinish(EventHandler action)
+        => finishHandler += action;
+}
+public class GeneralEventArgs<T> : EventArgs
+{
+    public T Value;
+    public GeneralEventArgs(T value) => Value = value;
 }
